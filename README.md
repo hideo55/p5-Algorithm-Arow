4e800a6b3a28bf9628d265041943dad450e9ea9b
hideo55 <hide.o.j55@gmail.com>
2014-06-26 23:49:08 +0900

    Update docs

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Algorithm::ArowPP - Implementation of AROW(Adaptive Regularization of Weights).
+
+# SYNOPSIS
+
+    use Algorithm::ArowPP;
+
+# DESCRIPTION
+
+Algorithm::ArowPP is ...
+
+# LICENSE
+
+Copyright (C) hideo55.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hideo55 <hide.o.j55@gmail.com>
---
+++ lib/Algorithm/ArowPP.pm
@@ -0,0 +1,41 @@
+package Algorithm::ArowPP;
+use 5.008005;
+use strict;
+use warnings;
+use XSLoader;
+
+BEGIN {
+    our $VERSION = '0.01';
+    XSLoader::load __PACKAGE__, $VERSION;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Algorithm::ArowPP - Implementation of AROW(Adaptive Regularization of Weights).
+
+=head1 SYNOPSIS
+
+    use Algorithm::ArowPP;
+
+=head1 DESCRIPTION
+
+Algorithm::ArowPP is ...
+
+=head1 LICENSE
+
+Copyright (C) hideo55.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hideo55 E<lt>hide.o.j55@gmail.comE<gt>
+
+=cut
+
